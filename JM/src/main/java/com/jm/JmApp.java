3d4194d0287239c22b293a368cc4a66922545293
MJH <x2twoi@naver.com>
2022-05-26 23:45:25 +0900

    1. Dev Setting

---
+++ JM/src/main/java/com/jm/JmApp.java
@@ -0,0 +1,13 @@
+package com.jm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmApp.class, args);
+	}
+
+}
